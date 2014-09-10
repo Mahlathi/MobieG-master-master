cb50c6cba59bc7d58e6720777d0f33abfd8aef85
akhona <Aycore411@gmail.com>
2014-09-10 15:26:12 +0200

    other

--- test/TestAdmin.scala
+++ test/TestAdmin.scala
@@ -1,27 +1,88 @@
 
 
 import org.scalatest.{BeforeAndAfter, FunSuite, FeatureSpec, GivenWhenThen}
-import people.Admin
+import people.{Facilitator, Admin}
 import repository.AdminRepository.AdminRepository
 
 import scala.slick.driver.MySQLDriver.simple._
+import org.scalatest.{GivenWhenThen, FeatureSpec}
 
+import scalaz.std.list
 
 
 /**
  * Created by joseph on 2014/09/03.
  */
-class TestAdmin extends FunSuite with BeforeAndAfter
-{
+class TestAdmin extends FeatureSpec with GivenWhenThen {
 
   val admin = TableQuery[AdminRepository]
 
-  implicit var session: Session = _
+  Database.forURL("jdbc:mysql://localhost:3306/mysql", driver = "com.mysql.jdbc.Driver", user = "root", password = "admin").withSession { implicit session =>
 
-  Database.forURL("jdbc:mysql://localhost:3306/mysql", driver = "com.mysql.jdbc.Driver", user = "root", password = "root").withSession {
-    implicit session =>
+   //Creating Table Admin
+   // ( admin.ddl ).create
 
 
+    //Testing data insertion
+    feature("Testing Admin")
+    {
+      info("Test insert")
+      scenario("Test Admin insert"){
+        Given("Admin")
+
+        //val adm = Admin("6")
+
+        //val value = admin.insert("6");
+
+        //assert( value == 1)
+      }
+    }
+
+
+    //Testing for extraction
+    feature("Testing Admin")
+    {
+      info("Testing read")
+      scenario("Reading Admin"){
+        Given("Admin")
+
+        val results = admin.list
+
+        assert(results.size == 3)
+      }
+    }
+
+
+    //Testing for updation
+    feature("Updating Admin")
+    {
+      info("Testing update")
+      scenario("Updating Admin"){
+        Given("Admin")
+
+        val rmadd = admin.findBy(_.id)
+        val adm = Admin(rmadd.toString)
+
+
+        //assert(results.size == 3)
+      }
+    }
+
+    //Testing for updation
+    feature("Deleting Admin")
+    {
+      info("Testing delete")
+      scenario("Deleting Admin"){
+        Given("Admin")
+
+         // val rmadd = admin.findBy(_.id)
+
+
+         // admin.deleteStatement(rmadd)
+
+        //assert(results.size == 3)
+      }
+    }
 
   }
 }
--- test/TestChannel.scala
+++ test/TestChannel.scala
@@ -1,6 +1,6 @@
 
 import org.scalatest.{BeforeAndAfter, FunSuite, GivenWhenThen, FeatureSpec}
-import people.Facilitator
+import people.{Admin, Facilitator}
 import scala.slick.driver.MySQLDriver.simple._
 import repository.ChannelRepository.ChannelRepository
 import repository.FacilitatorRepository.FacilitatorRepository
@@ -11,7 +11,7 @@ import scala.slick.lifted.TableQuery
 /**
  * Created by joseph on 2014/09/03.
  */
-class TestChannel extends FunSuite with BeforeAndAfter
+class TestChannel extends FeatureSpec with GivenWhenThen
 {
 
   val channel = TableQuery[ChannelRepository]
@@ -19,16 +19,56 @@ class TestChannel extends FunSuite with BeforeAndAfter
 
   Database.forURL("jdbc:mysql://localhost:3306/mysql", driver = "com.mysql.jdbc.Driver", user = "root", password = "admin").withSession { implicit session =>
 
+    //Creating tables
     //( channel.ddl ++ facilitator.ddl ).create
 
 
-    //val chan = Channel("1", "Akhona", "I am simply the best", "2")
+    feature("Testing channel")
+    {
+      info("Testing insert")
+      scenario("Read channel"){
+        Given("channel")
+          //val chan = Channel("1", "Akhona", "I am simply the best", "2")
 
-   // val fac = Facilitator("3")
+          // val fac = Facilitator("3")
 
-    val other = facilitator.insert("2")
-    val valo = channel.insert("1", "Akhona", "I am simply the best", "2")
+          //val other = facilitator.insert("3")
+          //val valo = channel.insert("5", "Akhona", "I am simply the best", other.toString)
+        //assert(results.size == 2)
+      }
+    }
 
+    //Testing for extraction
+    feature("Testing channel")
+    {
+      info("Testing read")
+      scenario("Reading channel"){
+        Given("channel")
+
+        val results = channel.list
+        //val otherresults = facilitator.list
+
+        assert(results.size == 3)
+        //assert(otherresults.size == 2)
+      }
+    }
+
+
+    //Testing for updation
+    feature("Updating channel")
+    {
+      info("Testing update")
+      scenario("Updating Admin"){
+        Given("Admin")
+
+
+        val rmadd = channel.findBy(_.id)
+        val walue = channel.insert( "" + rmadd., "Mahlathi", "used to be the best", "2")
+        val results = channel.list
+
+        assertResult( results.take(2), "Mahlathi")_
+      }
+    }
 
   }
 
